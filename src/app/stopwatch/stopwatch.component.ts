96a3eb295ffefc70fc357199c2352f9adb541e92
timc986 <timothychan986@hotmail.com>
2021-01-05 15:42:59 +0000

    stopwatch working

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from "rxjs";
 
 @Component({
   selector: 'app-stopwatch',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StopwatchComponent implements OnInit {
 
+  public isRunning = false;
+  public time = 0;
+  public hour = '00';
+  public minute = '00';
+  public second = '00';
+  public millisecond = '00';
+
   constructor() { }
 
   ngOnInit(): void {
+    timer(0, 10).subscribe(t => {
+      if (this.isRunning) {
+        this.time++;
+
+        this.hour = this.formatZero(Math.floor(this.time / 360000));
+        this.minute = this.formatZero(Math.floor(this.time / 6000 % 60));
+        this.second = this.formatZero(Math.floor(this.time / 100 % 60));
+        this.millisecond = Math.floor(this.time % 100).toString();
+      }
+    });
   }
 
+  public toggle(): void {
+    this.isRunning = !this.isRunning;
+  }
+
+  public reset(): void {
+    this.isRunning = false;
+    this.time = 0;
+    this.hour = '00';
+    this.minute = '00';
+    this.second = '00';
+    this.millisecond = '00';
+  }
+
+  private formatZero(time: number): string {
+    if (time < 10) {
+      return '0' + time.toString();
+    } else {
+      return time.toString();
+    }
+  }
 }
